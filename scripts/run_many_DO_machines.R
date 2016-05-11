43fa0b5ea0c933d9492f3042c88a65a5ffe6c56a
Petr Simecek <lamparna@gmail.com>
2016-05-11 09:41:25 -0400

    updated to IBANGS

--- scripts/run_many_DO_machines.R
+++ scripts/run_many_DO_machines.R
@@ -1,9 +1,9 @@
-library("analogsea")
+library(analogsea)
 library(parallel)
 library(doParallel)
 Sys.setenv(DO_PAT = "*** REPLACE THIS BY YOUR DIGITAL OCEAN API KEY ***")
 
-participants <- read.csv("participant_list_addiction_course.csv", as.is=TRUE)
+participants <- read.csv("participant_list_ibangs.csv", as.is=TRUE)
 N = nrow(participants)
 
 # create a droplet for each participant
@@ -13,7 +13,7 @@ droplet_list <- list()
 for(i in 1:N) {
   print(i)
   # start i-th machine
-  droplet_list[[i]] <- docklet_create(size = getOption("do_size", "8gb"),
+  droplet_list[[i]] <- docklet_create(size = getOption("do_size", "8gb"), 
                                       region = getOption("do_region", "nyc2"))
 }
 
@@ -29,7 +29,7 @@ foreach(i = 1:N, .packages="analogsea") %dopar% {
   
   # pull docker images
   d %>% docklet_pull("rocker/hadleyverse")
-  d %>% docklet_pull("churchill/doqtl")
+  d %>% docklet_pull("churchill/ibangs2016")
   d %>% docklet_images()
 }
 
@@ -54,14 +54,19 @@ for(i in 1:N) {
   # select droplet
   d = droplet_list[[i]]
   
-  d %>% docklet_run("-d", " -v /ibangs/data:/ibangs/data", " -v /ibangs/tutorial:/ibangs/tutorial"," -p 8787:8787", " -e USER=rstudio", " -e PASSWORD=ibangs ", "churchill/doqtl")
-  
+  d %>% docklet_run("-d", " -v /ibangs/data:/ibangs/data", " -v /ibangs/tutorial:/ibangs/tutorial", " -p 8787:8787", 
+                    " -e USER=rstudio", " -e PASSWORD=ibangs ", "--name myrstudio ", "churchill/ibangs2016")
+
+  # add symbolic links
+  lines2 <- "docker exec myrstudio ln -s /ibangs/data /home/rstudio/data
+             docker exec myrstudio ln -s /ibangs/tutorial /home/rstudio/tutorial"
+  cmd2 <- paste0("ssh ", analogsea:::ssh_options(), " ", "root", "@", analogsea:::droplet_ip(d)," ", shQuote(lines2))
+  analogsea:::do_system(d, cmd2, verbose = TRUE)  
 }
 
 ### Create participant table with links
 participants$DO_machine <- sapply(droplet_list, function(x) x$name)
 participants$link_RStudio <- sapply(droplet_list, function(x) paste0("http://",analogsea:::droplet_ip(x),":8787"))
-participants$link_terminal <- sapply(droplet_list, function(x) paste0("http://",analogsea:::droplet_ip(x),":43210"))
 
 library(xtable)
 sanitize.text.function <- function(x) {
@@ -69,7 +74,7 @@ sanitize.text.function <- function(x) {
   x[idx] <- paste0('<a href="',x[idx],'">',sub("^http://","",x[idx]),'</a>')
   x
 }
-cols <- c("BadgeName", "DO_machine", "link_RStudio", "link_terminal")
+cols <- c("Name", "DO_machine", "link_RStudio")
 print(xtable(participants[,cols], caption="Digital Ocean Machines"),
       type = "html", sanitize.text.function = sanitize.text.function,
       file = "dolist.html", include.rownames=FALSE)
@@ -79,18 +84,17 @@ library(mailR)
 
 
 for (i in 1:N) {
-  email_body <- paste0("Dear ",participants$first_Name[i],",\n\n",
-                       "During the workshop, you will need an access to RStudio and terminal ",
-                       "running on your personal Digital Ocean machine. You can access the machine ",
-                       "directly in your browser:\n\n",
-                       participants$link_RStudio[i]," (RStudio, user:rstudio, password:sysgen)\n",
-                       participants$link_terminal[i]," (terminal, user:root, password:sysgen)\n\n",
+  email_body <- paste0("Dear ",participants$Name[i],",\n\n",
+                       "During the workshop, you will need an access to RStudio Server",
+                       "running on your personal Digital Ocean virtual machine. You can access the machine ",
+                       "in your web browser:\n\n",
+                       participants$link_RStudio[i]," (RStudio, user:rstudio, password:sysgen)\n\n",
                        "After the workshop you can run this docker image either on your personal machine or host it ",
-                       "on Digital Ocean (as we did). Further instructions can be found on https://github.com/churchill-lab/sysgen2015.\n\n",
+                       "on Digital Ocean (as we did). Further instructions can be found on https://github.com/churchill-lab/IBANGS2016 or https://github.com/churchill-lab/sysgen2015.\n\n",
                        "Best regards,\n\n","Petr Simecek")
   
   send.mail(from = "REPLACE THIS BY YOUR_GMAIL@gmail.com",
-            to = participants$Email_Address[i],
+            to = participants$Email[i],
             replyTo = "REPLACE THIS BY YOUR_EMAIL",
             subject = "Short Course On The Genetics Of Addiction - Digital Ocean Machine Access",
             body = email_body,
